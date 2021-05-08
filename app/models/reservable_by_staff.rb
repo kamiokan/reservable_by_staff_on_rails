9624bc65a42e13a32798d51bc9c02983b617d588
kamiokan <spherenov@gmail.com>
2021-05-09 08:47:47 +0900

    reservable_by_staffにバリデーションを追加

--- app/models/reservable_by_staff.rb
+++ app/models/reservable_by_staff.rb
@@ -1,3 +1,7 @@
 class ReservableByStaff < ApplicationRecord
   belongs_to :staff
+  validates :start_time, absence: false
+  validates :start_time, format: { with: /\A\d{4}\z/, message: "数字４桁で入力してください" }
+  validates :end_time, absence: false
+  validates :end_time, format: { with: /\A\d{4}\z/, message: "数字４桁で入力してください" }
 end
