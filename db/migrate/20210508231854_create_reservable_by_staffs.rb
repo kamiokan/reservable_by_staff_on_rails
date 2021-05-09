1032aca0f2a299672755b817dcee6ada6de5a2d4
kamiokan <spherenov@gmail.com>
2021-05-09 09:18:06 +0900

    reservable_by_staffのseedを追加

--- db/migrate/20210508231854_create_reservable_by_staffs.rb
+++ db/migrate/20210508231854_create_reservable_by_staffs.rb
@@ -5,7 +5,7 @@ class CreateReservableByStaffs < ActiveRecord::Migration[6.1]
       t.string :start_time
       t.string :end_time
       t.references :staff, null: false, foreign_key: true
-      t.integer :filled_flag
+      t.integer :filled_flag, default: 0
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Staff.create(name: "花垣 武道")
-Staff.create(name: "橘 日向")
-Staff.create(name: "橘 直人")
-Staff.create(name: "佐野 万次郎")
-Staff.create(name: "龍宮寺 堅")
+Staff.create(name: '花垣 武道')
+Staff.create(name: '橘 日向')
+Staff.create(name: '橘 直人')
+Staff.create(name: '佐野 万次郎')
+Staff.create(name: '龍宮寺 堅')
+
+start_times = %w[1200 1215 1230 1245 1300 1315 1330 1345]
+end_times = %w[1215 1230 1245 1300 1315 1330 1345 1400]
+[1, 2, 3, 4, 5].each do |staff_id|
+  start_times.each_with_index do |time, index|
+    ReservableByStaff.create!(
+      the_date: '2021-05-08',
+      start_time: time,
+      end_time: end_times[index],
+      staff_id: staff_id,
+      filled_flag: [0.1].sample
+    )
+  end
+end
